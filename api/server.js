4d6c6d819c2f930050b40f66132c19867d8ae846
DarkedgeTH <darkedgeth@gmail.com>
2022-04-11 16:08:57 -0400

    debugged delete request

--- api/server.js
+++ api/server.js
@@ -5,14 +5,22 @@ const server = express()
 server.use(express.json())
 
 server.delete('/api/users/:id', async (req, res) => {
-    const possibleUser = await User.findById(req.params.id)
-    if (!possibleUser) {
-        res.status(404).json({
-            message: 'The user with the specified ID does not exist'
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else {
+            const deletedUser = await User.remove(possibleUser.id)
+            res.status(200).json(deletedUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error deleting user',
+            err: err.message,
+            stack: err.stack
         })
-    } else {
-        const deletedUser = await User.remove(possibleUser.id)
-        res.status(200).json(deletedUser)
     }
 })
 
